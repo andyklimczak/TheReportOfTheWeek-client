8f7a65125842f11ec00552bc53fe166f9b377aff
Andy Klimczak <andyklimczak@fastmail.com>
2016-11-01 22:01:26 -0400

    Start rating chart stuff

--- src/components/RatingChart.js
+++ src/components/RatingChart.js
@@ -8,10 +8,22 @@ class RatingChart extends Component {
   render() {
     return (
       <VictoryChart>
+        <VictoryAxis
+          tickCount={10}
+          style={{
+            ticks: {stroke: "black", strokeWidth: 3}
+          }}
+        />
+        <VictoryAxis dependentAxis
+          tickCount={4}
+          domain={[0, 10]}
+          label="Rating"
+        />
         <VictoryScatter
           data={this.props.data}
           x="dateReleased"
           y="rating"
+          size={1}
         />
       </VictoryChart>
     );
