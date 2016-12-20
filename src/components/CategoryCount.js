39b9b41ca48a925ecbd071855de933123b3a30cd
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-20 00:19:43 -0500

    something of a bar chart in recharts

--- src/components/CategoryCount.js
+++ src/components/CategoryCount.js
@@ -1,83 +1,30 @@
 import React, { Component, PropTypes } from 'react';
 import { VictoryPie, Slice, VictorySharedEvents, VictoryBar, VictoryLabel } from 'victory';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 import '../assets/css/ListItemForm.css';
 
 class CategoryCount extends Component {
   render() {
     return (
-      <svg viewBox="0 0 450 170">
-        <VictorySharedEvents
-          events={[{
-            childName: ["pie", "bar"],
-            target: "data",
-            eventHandlers: {
-              onMouseOver: () => {
-                return [{
-                  childName: ["pie", "bar"],
-                  mutation: (props) => {
-                    return {
-                      style: Object.assign({}, props.style, {fill: "tomato"})
-                    };
-                  }
-                }, {
-                  childName: ["pie"],
-                  target: "labels",
-                  mutation: (props) => {
-                    return {
-                      style: Object.assign({}, props.style, {fill: "black"}),
-                    };
-                  }
-                }];
-              },
-              onMouseOut: () => {
-                return [{
-                  childName: ["pie", "bar"],
-                  mutation: () => {
-                    return null;
-                  }
-                }, {
-                  childName: ["pie"],
-                  target: "labels",
-                  mutation: () => {
-                    return null;
-                  }
-                }];
-              }
-            }
-          }]}
-        >
-          <g transform={"translate(170, 0)"}>
-            <VictoryBar name="bar"
-              width={200}
-              height={200}
-              standalone={false}
-              style={{
-                data: { width: 10 },
-                labels: {fontSize: 3}
-              }}
-              labels={(datum) => `${datum.category}`}
-              data={this.props.data}
-              x="category"
-              y="count"
-              labelComponent={<VictoryLabel y={160}/>}
-            />
-          </g>
-          <VictoryPie
-            name="pie"
-            standalone={false}
-            data={this.props.data}
-            x="category"
-            y="count"
-            height={200}
-            width={200}
-            labels={(datum) => `${datum.count} ${datum.count > 1 ? 'reviews' : 'review'}`}
-            style={{
-              labels: {fontSize: 3, fill: "transparent"},
-            }}
+      <div className="category-count">
+        <BarChart
+          width={800}
+          height={400}
+          data={this.props.data}>
+          <Tooltip />
+          <XAxis
+            dataKey="category"
           />
-        </VictorySharedEvents>
-      </svg>
+          <Bar
+            barSize={60}
+            name="Reviews"
+            dataKey="count"
+            minPointSize={5}
+            fill="salmon"
+          />
+        </BarChart>
+      </div>
     );
   }
 }
