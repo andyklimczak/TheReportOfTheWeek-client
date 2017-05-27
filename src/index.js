b654ae290ed4f48cc504554ee3eeb58ff5770e7e
Andy Klimczak <andyklimczak@fastmail.com>
2017-05-26 21:40:59 -0400

    add analytics *

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Root from './containers/Root';
 import configureStore from './store/configureStore';
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import ReactGA from 'react-ga';
 
 import './assets/css/index.css';
 
@@ -11,7 +12,14 @@ import './assets/css/index.css';
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
+const logPageView = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
+
+ReactGA.initialize('UA-100026494-1');
+
 render(
-  <Root store={store} history={history} />,
+  <Root store={store} history={history} onUpdate={logPageView} />,
   document.getElementById('root')
 );
